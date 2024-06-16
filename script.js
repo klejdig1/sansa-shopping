a1631e0b2e1bea14b24a73f8830dfc4bbb29c3a6
klejdig1 <klejdiwork@gmail.com>
2024-06-16 18:52:32 +0200

    page-profile 90%, no responsive

--- script.js
+++ script.js
@@ -4,3 +4,48 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var inputButton = document.querySelector(
+    '.placeholder-butt input[type="button"]',
+  );
+  var placeholderText = "Chat with us";
+  // Set placeholder text initially
+  inputButton.value = placeholderText;
+
+  inputButton.addEventListener("click", function () {
+    if (inputButton.value === placeholderText) {
+      inputButton.value = "";
+      inputButton.classList.add("has-value");
+    }
+  });
+
+  inputButton.addEventListener("blur", function () {
+    if (inputButton.value === "") {
+      inputButton.value = placeholderText;
+      inputButton.classList.remove("has-value");
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var inputButton = document.querySelector(
+    '.placeholder-butt2 input[type="button"]',
+  );
+  var placeholderText = "Log Out";
+  // Set placeholder text initially
+  inputButton.value = placeholderText;
+  inputButton.addEventListener("click", function () {
+    if (inputButton.value === placeholderText) {
+      inputButton.value = "";
+      inputButton.classList.add("has-value");
+    }
+  });
+
+  inputButton.addEventListener("blur", function () {
+    if (inputButton.value === "") {
+      inputButton.value = placeholderText;
+      inputButton.classList.remove("has-value");
+    }
+  });
+});
